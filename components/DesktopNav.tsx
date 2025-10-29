66f6703eaf3e52e69a5611842caa8cc76d94b33b
“bendiagi <ebenezerdiagi@gmail.com>
2025-10-29 20:34:50 +0100

    Mobile polish: reduce navbar divider thickness (border-b-2 on mobile, desktop unchanged).

--- components/DesktopNav.tsx
+++ components/DesktopNav.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const DesktopNav: React.FC = () => {
 
   return (
-    <nav className="flex w-full lg:w-[95vw] mx-auto bg-[#222821] border-b-4 lg:border-b-2 border-[#DAE5D0] rounded-none lg:rounded-b-[3rem] p-4 shadow-lg mb-0">
+    <nav className="flex w-full lg:w-[95vw] mx-auto bg-[#222821] border-b-2 lg:border-b-2 border-[#DAE5D0] rounded-none lg:rounded-b-[3rem] p-4 shadow-lg mb-0">
       <div className="flex items-center justify-between w-full">
         {/* Left side - Title + Subtitle */}
         <div className="flex flex-col pl-2 lg:pl-4">
